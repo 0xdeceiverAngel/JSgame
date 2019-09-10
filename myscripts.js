008648d5bd26d86e78504e15d11b1b638905c582
行者 <31663805+0xdeciverAngel@users.noreply.github.com>
2019-09-11 00:06:00 +0800

    add many function and complete dev

--- myscripts.js
+++ myscripts.js
@@ -2,17 +2,22 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var player_x=canvas.width/2-150;
 var player_y=canvas.height/2;
-
+var gen_rate=1000;
 var enemy_arrary=[];
 var upPressed = false;
 var downPressed = false;
 var pos=canvas.width;
-
+var score=0;
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+document.addEventListener("mousemove", mouseMoveHandler, false);
+function mouseMoveHandler(e) {
+    var relativeY = e.clientY ;
+    if(relativeY > 0 && relativeY < canvas.height-20) {
+       player_y=relativeY;
+    }
 }
+
 function keyDownHandler(e) {
     if(e.key == "Up" || e.key == "ArrowUp") {
         upPressed = true;
@@ -55,6 +60,34 @@ function draw_player()
         }
  }
 }
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#aaa";
+    ctx.fillText("Score: "+score, 8, 20);
+}
+function remove_enemy()
+{
+  
+  for(var i=0;i<enemy_arrary.length;i++)
+    {
+      
+      if(!enemy_arrary[i].x)
+        {
+          enemy_arrary.splice(i, 1);
+        }
+     
+    }
+}
+function count_score()
+{
+for(var i=0;i<enemy_arrary.length;i++)
+    {  if(enemy_arrary[i].x+20<player_x&&enemy_arrary[i].saw!=1)
+    {
+      score+=1;
+      enemy_arrary[i].saw=1;
+    }
+    }
+}
 function modify_enemy_x_pos()
 {
   for(var i=0;i<enemy_arrary.length;i++)
@@ -63,13 +96,59 @@ function modify_enemy_x_pos()
 
     }
 }
-async function gen_enemy()
+function check_gameover()
+{
+  for(var i=0;i<enemy_arrary.length;i++)
+    {  
+      if(enemy_arrary[i].y<=player_y&&player_y<=enemy_arrary[i].y+20&&enemy_arrary[i].x<=player_x&&player_x<=enemy_arrary[i].x+20){
+//         console.log("down");
+        alert("GAME OVER");
+    document.location.reload();
+   
+      }
+      
+    if(player_y<=enemy_arrary[i].y&&enemy_arrary[i].y<=player_y+20&&
+      player_x<=enemy_arrary[i].x&&enemy_arrary[i].x<=player_x+20
+      ){
+//         console.log("down");
+    alert("GAME OVER");
+    document.location.reload();
+    
+      }  
+      
+    }
+}
+function gen_enemy()
 {
 //   await sleep(800);
-    var enemy_y=Math.floor(Math.random()*canvas.height);
+    var enemy_y=Math.floor(Math.random()*(canvas.height-20));
     var enemy_x=canvas.width-1;
     var obj={x:enemy_x,y:enemy_y};
     enemy_arrary.push(obj);
+}
+function gen_rate_speedup()
+{
+  if(score==5&&gen_rate!=500)
+    {
+      gen_rate=500;
+      clearInterval(interval);
+      interval=setInterval(gen_enemy, gen_rate);
+        
+    }
+  if(score==10&&gen_rate!=200)
+    {
+      gen_rate=200;
+      clearInterval(interval);
+      interval=setInterval(gen_enemy, gen_rate);
+    }
+  if(score==100&&gen_rate!=100)
+    {
+      gen_rate=100;
+    clearInterval(interval);
+      interval=setInterval(gen_enemy, gen_rate);
+
+        
+    }
 }
 function draw_enemy()
     {
@@ -94,13 +173,17 @@ function draw()
 {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   draw_player();
-//   gen_enemy()
   draw_enemy();
   modify_enemy_x_pos();
-//   console.log(enemy_arrary[0]);
+  remove_enemy();
+  count_score();
+  drawScore();
+  check_gameover();
+  gen_rate_speedup();
+
   
 
 }
 
 setInterval(draw, 10);
-setInterval(gen_enemy, 1000);
+var interval=setInterval(gen_enemy, gen_rate);
